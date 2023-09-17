1f10eabfe566d791126e7600aa36e87969505168
ProfNava <145283190+ProfNava@users.noreply.github.com>
2023-09-17 11:00:01 -0600

    Update boot.py

--- pub-sub-client-ESP32/boot.py
+++ pub-sub-client-ESP32/boot.py
@@ -8,7 +8,7 @@ import time
 print("Connecting to WiFi", end="")
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
-sta_if.connect('MEGACABLE-AB9F', 'mamapapatania')
+sta_if.connect('RED WIFI', 'PASSWORD')
 while not sta_if.isconnected():
   print(".", end="")
   time.sleep(0.25)
